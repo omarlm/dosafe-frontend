2626ef630ba7b68cb56fd97098126f1de8d964a2
Omar Lorenzo <omarlm@MacBook-Pro-de-Omar.local>
2024-04-09 12:56:51 +0100

    Added .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,15 @@
-import { IDisplayPassword } from '../interfaces/IDisplayPassword'
+import { IDisplayPassword } from '../interfaces/IDisplayPassword';
 
 export const generatePassword = async (language: string): Promise<string> => {
     try {
-        const response = await fetch(`http://localhost:3001/generate-password/${language}`)
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/generate-password/${language}`);
         if (!response.ok) {
-            throw new Error('Respuesta del servidor no fue exitosa')
+            throw new Error('Respuesta del servidor no fue exitosa');
         }
-        const data: IDisplayPassword = await response.json()
-        return data.password
+        const data: IDisplayPassword = await response.json();
+        return data.password;
     } catch (error) {
-        console.log(error)
-        throw error
+        console.log(error);
+        throw error;
     }
-}
+};
